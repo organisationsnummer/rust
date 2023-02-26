bdeaa9b6f2e033a9a32dbb04bcdf60a17acf6914
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2023-02-26 15:59:10 +0100

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,215 @@
+#[macro_use]
+extern crate lazy_static;
+
+use regex::{Match, Regex};
+use std::{convert::TryFrom, error::Error, fmt};
+
+lazy_static! {
+    static ref ORG_REGEX: Regex = Regex::new(
+        r"(?x)^(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([-+]?)?(\d{3})(\d)$"
+    )
+    .unwrap();
+}
+
+#[derive(Debug)]
+pub enum OrganisationsnummerError {
+    InvalidInput,
+}
+
+impl fmt::Display for OrganisationsnummerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            OrganisationsnummerError::InvalidInput => write!(f, "Invalid format"),
+        }
+    }
+}
+
+impl Error for OrganisationsnummerError {}
+
+/// Organisationsnummer holds relevant data to check for valid organization numbers.
+pub struct Organisationsnummer {
+    number: String,
+}
+
+impl TryFrom<&str> for Organisationsnummer {
+    type Error = OrganisationsnummerError;
+
+    fn try_from(org: &str) -> Result<Self, OrganisationsnummerError> {
+        let caps = ORG_REGEX
+            .captures(org)
+            .ok_or(OrganisationsnummerError::InvalidInput)?;
+
+        let match_to_u32 =
+            |m: Option<Match<'_>>| -> u32 { m.unwrap().as_str().parse::<u32>().unwrap_or(0) };
+
+        let prefix = match caps.get(1) {
+            Some(m) => match_to_u32(Some(m)),
+            None => 0
+        };
+
+        // May only be prefixed with 16.
+        if prefix != 0 && prefix != 16 {
+            return Err(OrganisationsnummerError::InvalidInput);
+        }
+
+        let third = match_to_u32(caps.get(3));
+
+        // Third digit bust be more than 20.
+        if third < 20 {
+            return Err(OrganisationsnummerError::InvalidInput);
+        }
+
+        let second = match_to_u32(caps.get(2));
+
+        // Second digit may not start with leading 0.
+        if second < 10 {
+            return Err(OrganisationsnummerError::InvalidInput);
+        }
+        // Luhn checksum must be valid.
+        if !luhn(org.to_string().replace("-", "")) {
+            return Err(OrganisationsnummerError::InvalidInput);
+        }
+
+        return Ok(Organisationsnummer{
+            number: org.to_string().replace("-", ""),
+        });
+    }
+}
+
+impl Organisationsnummer {
+    /// Returns a new instance of a Organisationsnummer.
+    pub fn new(org: &str) -> Result<Organisationsnummer, OrganisationsnummerError> {
+        Organisationsnummer::try_from(org)
+    }
+
+    /// Same as new().
+    pub fn parse(org: &str) -> Result<Organisationsnummer, OrganisationsnummerError> {
+        Organisationsnummer::try_from(org)
+    }
+
+    /// Validate a Organisationsnummer. The validation requires a valid Luhn checksum.
+    pub fn valid(&self) -> bool {
+        true
+    }
+
+    // Format organization number with or without separator.
+    pub fn format(&self, separator: Option<bool>) -> String {
+        if separator.unwrap_or(true) {
+            return format!("{}-{}",&self.number[..6],&self.number[6..])
+        }
+
+        return self.number.clone()
+    }
+
+    // Get the organization type.
+    pub fn r#type(&self) -> String {
+        let first = self.number.chars().next().unwrap().to_digit(10).unwrap_or(0);
+        let r#type = match first {
+            1 => "Dödsbon",
+            2 => "Stat, landsting, kommun eller församling",
+            3 => "Utländska företag som bedriver näringsverksamhet eller äger fastigheter i Sverige",
+            5 => "Aktiebolag",
+            6 => "Enkelt bolag",
+            7 => "Ekonomisk förening eller bostadsrättsförening",
+            8 => "'Ideella förening och stiftelse",
+            9 => "Handelsbolag, kommanditbolag och enkelt bolag",
+            _ => "Okänt",
+        };
+        r#type.to_string()
+    }
+
+    // Get organization vat number.
+    pub fn vat_number(&self) -> String {
+        return format!("SE{}01", self.number);
+    }
+}
+
+/// Determine if the checksum based on luhn algorithm is valid. See more information here:
+/// https://en.wikipedia.org/wiki/Luhn_algorithm.
+fn luhn(value: String) -> bool {
+    let checksum = value
+        .chars()
+        .map(|c| c.to_digit(10).unwrap_or(0))
+        .enumerate()
+        .fold(0, |acc, (idx, v)| {
+            let value = if idx % 2 == 0 { v * 2 } else { v };
+            acc + if value > 9 { value - 9 } else { value }
+        });
+
+    (10 - (checksum as u8 % 10)) % 10 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_invalid_input() {
+        let cases = vec!["556016-0681", "556103-4250", "5561034250"];
+
+        for tc in cases {
+            assert!(Organisationsnummer::parse(tc).is_err());
+        }
+    }
+
+    #[test]
+    fn test_valid_organization_numbers() {
+        let cases = vec!["556016-0680", "556103-4249", "5561034249", "559244-0001"];
+
+        for tc in cases {
+            assert!(Organisationsnummer::parse(tc).unwrap().valid());
+        }
+    }
+
+    #[test]
+    fn test_valid_limited_companies() {
+        let cases = vec!["556016-0680", "556103-4249", "5561034249", "559244-0001"];
+
+        for tc in cases {
+            assert_eq!(Organisationsnummer::parse(tc).unwrap().r#type(), "Aktiebolag");
+        }
+    }
+
+    #[test]
+    fn test_valid_format_with_separator() {
+        let cases = HashMap::from([
+            ("559244-0001","559244-0001"),
+            ("556016-0680", "556016-0680"),
+            ("556103-4249", "556103-4249"),
+            ("5561034249", "556103-4249"),
+        ]);
+
+        for (key, value) in cases {
+            assert_eq!(Organisationsnummer::parse(key).unwrap().format(None), value);
+        }
+    }
+
+    #[test]
+    fn test_valid_format_without_separator() {
+        let cases = HashMap::from([
+            ("559244-0001","5592440001"),
+            ("556016-0680", "5560160680"),
+            ("556103-4249", "5561034249"),
+            ("5561034249", "5561034249"),
+        ]);
+
+        for (key, value) in cases {
+            assert_eq!(Organisationsnummer::parse(key).unwrap().format(Some(false)), value);
+        }
+    }
+
+    #[test]
+    fn test_valid_vat_numbers() {
+        let cases = HashMap::from([
+            ("559244-0001","SE559244000101"),
+            ("556016-0680", "SE556016068001"),
+            ("556103-4249", "SE556103424901"),
+            ("5561034249", "SE556103424901"),
+        ]);
+
+        for (key, value) in cases {
+            assert_eq!(Organisationsnummer::parse(key).unwrap().vat_number(), value);
+        }
+    }
+}
