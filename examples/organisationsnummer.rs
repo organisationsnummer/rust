de7db1d3909d4ccf83f8f714c22c9fbea37e5a5a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2023-03-10 18:22:43 +0100

    Revert change in example

--- examples/organisationsnummer.rs
+++ examples/organisationsnummer.rs
@@ -1,19 +1,21 @@
-use personnummer::{Personnummer, PersonnummerError};
+use organisationsnummer::{Organisationsnummer, OrganisationsnummerError};
 use std::env;
 
-fn main() -> Result<(), PersonnummerError> {
+fn main() -> Result<(), OrganisationsnummerError> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: cargo run --example organisationsnummer <organisationsnummer>");
-        return Err(PersonnummerError::InvalidInput);
+        return Err(OrganisationsnummerError::InvalidInput);
     }
 
-    let org = Personnummer::new(&args[1])?;
+    let org = Organisationsnummer::new(&args[1])?;
 
     if org.valid() {
         println!(
-            "The company with organization number {}",
+            "The company with organization number {} is a {} and the vat number is {}",
             org.format().long(),
+            org.r#type(),
+            org.vat_number()
         );
     } else {
         println!("invalid organization number provided");
